0a836bfc373801ff9a617a9cc3433115340501b6
ZLange <lange.zaiga@gmail.com>
2020-12-12 20:24:29 +0200

    Variables

---
+++ variables.py
@@ -0,0 +1,21 @@
+# Variables
+
+user_iq = 190 # variable
+
+user_age = user_iq/4
+aa = user_age
+
+
+print(aa)
+
+# keywords -> https://www.w3schools.com/python/python_ref_keywords.asp
+
+# constants -> things that never change, write in capitals
+PI = 3.14
+
+print('********************************')
+# add more than one value to more than one variable
+a, b, c = 1,2,3
+print(a)
+print(b)
+print(c)
