21563054a8c82e48a83a895c1167d8534b853aeb
ZLange <lange.zaiga@gmail.com>
2020-12-12 19:18:32 +0200

    Data Types info

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
-# test rep
-
-print('Learning Python is fun!!')
+# # test rep
+#
+# # Fundamental Data types - core to the language
+# int
+# float
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+#
+# # Classes -> custom types
+#
+# # Specialized Data types -> extra boosts
+#
+# None
+#
