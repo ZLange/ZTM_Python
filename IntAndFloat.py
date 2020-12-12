69954d33684e0c2501d70c6def1a717a8b77ef38
ZLange <lange.zaiga@gmail.com>
2020-12-12 19:42:53 +0200

    int and float + math functions

---
+++ IntAndFloat.py
@@ -0,0 +1,16 @@
+# int and float
+
+print(type(2+4))
+print(type(2-4))
+print(type(2*4))
+print(type(2/4)) # 0.5
+
+print(2 ** 2)
+print(2 // 4)
+print(5 % 4) # modulo? gives reminder, in this case 1
+
+# math functions
+
+print(round(3.9))
+print(abs(-20))
+
