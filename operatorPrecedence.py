a5f1da60d1755c25d0808f2a6a8c399ddcaa8107
ZLange <lange.zaiga@gmail.com>
2020-12-12 19:51:44 +0200

    Operator Precedence/ added Exercise

--- operatorPrecedence.py
+++ operatorPrecedence.py
@@ -1,12 +1,25 @@
 # Operator Precedence -> oder of actions
 
-print(20 + 3 * 4)
-print(20 - 3 * 4)
-print((20 -3) + 2 ** 2)
+# print(20 + 3 * 4)
+# print(20 - 3 * 4)
+# print((20 -3) + 2 ** 2)
 
 # order of actions:
 # ()
-# **
+# ** //
 # * /
 # + -
 
+# Exercise
+# Guess the output of each answer before you click RUN
+# Try to write down your answer before and see how you do... keep it mind I made it a little tricky for you :)
+
+print((5 + 4) * 10 / 2) # 45
+
+print(((5 + 4) * 10) / 2) # 45
+
+print((5 + 4) * (10 / 2)) # 45
+
+print(5 + (4 * 10) / 2) # 25
+
+print(5 + 4 * 10 // 2) # 25
