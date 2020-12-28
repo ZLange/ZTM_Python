48eb57aed207f8a53a08081fc6b348e11036916b
ZLange <lange.zaiga@gmail.com>
2020-12-28 14:19:14 +0200

    strings intro

---
+++ strings.py
@@ -0,0 +1,28 @@
+# Strings
+
+string_value = "Divas kepas"
+string_value2 = 'Diena kepa'
+
+print(string_value)
+print(string_value2)
+
+username = 'superuser'
+password = 'supersecret'
+
+long_string = '''
+WOW
+O O
+___
+,,,,
+
+'''
+
+print(long_string)
+
+
+first_name = 'Zaiga'
+last_name = 'Lange'
+full_name = first_name + ' ' + last_name
+
+print(full_name)
+
