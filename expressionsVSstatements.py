a44fb419ef654e436fa78800eda66417a9437645
ZLange <lange.zaiga@gmail.com>
2020-12-28 13:43:22 +0200

    augmented assigment operator + string file

--- expressionsVSstatements.py
+++ expressionsVSstatements.py
@@ -3,3 +3,9 @@ iq = 100
 user_age = iq / 5
 # expression is iq /5
 # statement is a line of code user_age = iq / 5
+
+
+# augmented assigment operator
+some_value = 5
+some_value +=  2
+print(some_value)
