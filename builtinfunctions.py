624de9f9c5861531a27280775d4002c77198e1cd
ZLange <lange.zaiga@gmail.com>
2021-02-28 14:11:18 +0200

    Build in functions and Methods

---
+++ builtinfunctions.py
@@ -0,0 +1,18 @@
+# Build in functions and Methods
+
+# great = 'helloooooo'
+#
+# # print(great[0:len(great)])
+#
+# print(great.capitalize())
+# print(great.swapcase())
+
+quote = 'to be or not to be'
+quote2 = quote.replace('be', 'me')
+
+print(quote.upper())
+print(quote.capitalize())
+print(quote.find('be'))
+print(quote.replace('be', 'me'))
+print(quote)
+print(quote2)
