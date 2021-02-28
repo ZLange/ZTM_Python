fe861273ee16b487f92329450c2824c4b0fb509f
ZLange <lange.zaiga@gmail.com>
2021-02-28 13:37:16 +0200

    Formatted strings

--- strings2.py
+++ strings2.py
@@ -1,8 +1,16 @@
-# Escape Sequence
+# # Escape Sequence
+#
+# weather = "\t It\'s \"kind of\" sunny \n hope you have a good day"
+# # \t adds tab before string
+# # \n adds text in new line
+#
+# print(weather)
+#
 
-weather = "\t It\'s \"kind of\" sunny \n hope you have a good day"
-# \t adds tab before string
-# \n adds text in new line
+# Formatted strings
 
-print(weather)
+name = 'Zaiga'
+age = 30
+
+print(f'Hi {name}. Youe are {age} old')
 
